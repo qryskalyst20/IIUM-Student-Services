a17e6dbded26ab7bbf5c6e62551e480ef0f58bf7
Qagura <quddusnuriman@gmail.com>
2023-05-06 20:42:28 +0800

    added font

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,16 +1,48 @@
 import React from "react";
-import { SafeAreaView, View, Text } from "react-native";
+import { SafeAreaView, View, Text, StyleSheet } from "react-native";
 // import SearchBar from "../../components/SearchBar";
 import NavigationBar from "../../components/SearchBar/NavigationBar";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
+SplashScreen.preventAutoHideAsync();
 const HomeScreen = () => {
+  const [fontsLoaded] = useFonts({
+    "SF-Pro-Text-Bold": require("../../../assets/fonts/SF-Pro-Text-Bold.otf"),
+    "SF-Pro-Text-Medium": require("../../../assets/fonts/SF-Pro-Text-Medium.otf"),
+    "Nexa-Heavy": require("../../../assets/fonts/Nexa-Heavy.ttf"),
+    "Nexa-ExtraLight": require("../../../assets/fonts/Nexa-ExtraLight.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <SafeAreaView className="flex-1 items-center bg-[#121212]">
+    <SafeAreaView
+      onLayout={onLayoutRootView}
+      className="flex-1 items-center bg-[#121212]"
+    >
       {/* <SearchBar placeholder={"Search"} /> */}
       <View className="bg-[#30D5C8] w-screen h-[20%] flex">
         <View className="m-[10%]">
-          <Text className="text-white text-2xl font-bold">Hello Danish!</Text>
-          <Text className="text-white font-semibold">
+          <Text
+            style={{ fontFamily: "SF-Pro-Text-Bold" }}
+            className="text-white text-2xl drop-shadow-2xl"
+          >
+            Hello Danish!
+          </Text>
+          <Text
+            style={{ fontFamily: "SF-Pro-Text-Medium" }}
+            className="text-white drop-shadow-2xl"
+          >
             Welcome back! You are missed!
           </Text>
         </View>
